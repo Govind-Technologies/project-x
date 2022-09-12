9dfbc268005253ca798b783b58b630a2217d6c3c
Jageshwar Sah <jageshwar@ezdatechnology.com>
2022-09-13 04:10:46 +0530

    feat: Base module for kotlin #4
    - Added Logger Interface
    - Added App Initializers interface
    - Added Invoke Status sealed class
    - Added Coroutine Core data class

---
+++ base/src/main/java/dev/govind/project/x/base/InvokeStatus.kt
@@ -0,0 +1,6 @@
+package dev.govind.project.x.base
+
+sealed class InvokeStatus
+object InvokeStarted : InvokeStatus()
+object InvokeSuccess : InvokeStatus()
+data class InvokeError(val throwable: Throwable) : InvokeStatus()
